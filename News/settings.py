c99f67e97b242f3fc3bb0989a90276b7cf78c644
L-S-2020 <leostewal@gmail.com>
2024-02-26 16:56:35 +0100

    Azure push final

--- News/settings.py
+++ News/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
 
