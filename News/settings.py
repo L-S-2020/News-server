b4f8d136bfffab353d350c86592dbd3c39507ed2
L-S-2020 <leostewal@gmail.com>
2024-06-08 17:52:51 +0200

    added pictures for readme

--- News/settings.py
+++ News/settings.py
@@ -164,9 +164,9 @@ MESSAGE_TAGS = {
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'stegle.eu'
+EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'news_website'
-EMAIL_HOST_PASSWORD = '6@uwDgbt@8sG!hoY6Zs2'
-DEFAULT_FROM_EMAIL = 'Account@news.stegle.eu'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = ''
